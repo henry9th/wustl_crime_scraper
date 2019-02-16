cb03370063dd9fc335bf74d9664914940a7949d8
Hyunrae Kim <hyunrae.kim@gmail.com>
2019-02-16 02:40:26 -0600

    Best I can do at this point

--- src/wustl_crime_scraper/Scraper.java
+++ src/wustl_crime_scraper/Scraper.java
@@ -1,6 +1,8 @@
 package wustl_crime_scraper;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URLEncoder;
@@ -17,6 +19,12 @@ import com.gargoylesoftware.htmlunit.html.FrameWindow;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.util.Vector;
 
@@ -44,9 +52,14 @@ public class Scraper {
 	private static WebClient client;
 
 	public static String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-	public static String[] monthsAb = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
-	
-	
+	public static String[] monthsAb = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT,", "NOV", "DEC"};
+	public static String[] monthsAbAlt = {"JAN.", "FEB.", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUG", "SEPT", "OCT", "NOV", "DEC"};
+	public static String[] monthsAbAlt2 = {"JAN.", "FEB.", "MARCH", "APR.", "MAY", "JUNE", "JULY", "AUG", "SEPT", "Oct.", "NOV", "DEC"};
+
+	public static int numTimeSkipped =0;
+	public static int numDateSkipped =0;
+	public static int numLocationSkipped =0;
+	public static int numSummarySkipped =0;
 
 	public static List<Crime> scrape(int month, int year) {
 		client = new WebClient();
@@ -68,7 +81,7 @@ public class Scraper {
 				}
 				count++;
 			}
-			
+
 			String lines[] = targetText.split("\\r?\\n");
 			LinkedList<String> filteredList = new LinkedList<String>(); 
 			for (int i = 2; i < lines.length; i++) { 
@@ -76,13 +89,13 @@ public class Scraper {
 					filteredList.add(lines[i]);
 				}
 			}
-			
+
 			LinkedList<String> splitList = new LinkedList<String>(); 
 			int begin = 0; 
-			
+
 			for (int i = 0; i < filteredList.size(); i++) { 
 				String line = filteredList.get(i); 
-				if (Character.isUpperCase(line.charAt(0)) && Character.isUpperCase(line.charAt(1))) {
+				if (Character.isUpperCase(line.charAt(0)) && Character.isUpperCase(line.charAt(1)) && !line.contains("SUSPECT")) {
 					if (i != 0) { 
 						String combined = ""; 
 						//System.out.println("BEGIN: " + begin);
@@ -96,30 +109,93 @@ public class Scraper {
 					}
 				}
 			}
-			
+
 			List<Crime> crimes = new LinkedList<Crime>(); 
-			
+
 			for (int i = 0; i < splitList.size(); i++) { 
 				String paragraph = splitList.get(i);
-				String type = paragraph.substring(0, paragraph.indexOf(" ")); 
-				String date = paragraph.substring(paragraph.indexOf(monthsAb[month]), paragraph.indexOf(""+year)+4);
-				String afterYear = paragraph.substring(paragraph.indexOf(year+""));
-				String time = afterYear.substring(afterYear.indexOf(":")-2, afterYear.indexOf(":") + 3);
-				String afterLocation = paragraph.substring(paragraph.indexOf("Location: ")+10); 
-				String location;
-				if (afterLocation.contains("(")) { 
-					location = afterLocation.substring(0, afterLocation.indexOf("("));
-				} else { 
-					location = afterLocation.substring(0, afterLocation.indexOf("Summary"));
+				String type = ""; 
+				if (Character.isUpperCase(paragraph.substring(paragraph.indexOf(" ")).charAt(0)) && Character.isUpperCase(paragraph.substring(paragraph.indexOf(" ")).charAt(1))) { 
+					type = paragraph.substring(0, paragraph.substring(paragraph.indexOf(" ")).indexOf(" "));
+				} else {
+					type = paragraph.substring(0, paragraph.indexOf(" ")); 
+				}
+	
+				String date = ""; 
+				String summary = ""; 
+				String time = ""; 
+				String location = ""; 
+				
+				if (paragraph.contains("No criminal incidents reported")  || paragraph.contains("No reports filed") || paragraph.contains("NO CRIMINAL INCIDENTS REPORTED") || paragraph.contains("No incidents reported") || paragraph.contains("No reports filled") || paragraph.contains("Drawn in error")) {
+					continue;
 				}
 				
-				String summary = paragraph.substring(paragraph.indexOf("Summary: ") + 9);
+				try {
+					if (paragraph.toLowerCase().indexOf(monthsAb[month].toLowerCase()) == -1) { 
+						if (paragraph.toLowerCase().indexOf(monthsAbAlt[month].toLowerCase()) == -1) { 
+							 date = paragraph.substring(paragraph.toLowerCase().indexOf(monthsAbAlt2[month].toLowerCase()), paragraph.indexOf(""+year)+4);
+						}
+						else {
+						 date = paragraph.substring(paragraph.toLowerCase().indexOf(monthsAbAlt[month].toLowerCase()), paragraph.indexOf(""+year)+4);
+						}
+					} else {
+						 date = paragraph.substring(paragraph.toLowerCase().indexOf(monthsAb[month].toLowerCase()), paragraph.indexOf(""+year)+4);
+					}
+				} catch (Exception e) { 
+					System.out.println("date error");
+					e.printStackTrace();
+					System.out.println(paragraph.toLowerCase());
+					System.out.println(monthsAb[month].toLowerCase());
+					numDateSkipped++;
+				}
+				
+				try { 
+					String afterYear = paragraph.substring(paragraph.indexOf(year+""));
+					time = afterYear.substring(afterYear.indexOf(":")-2, afterYear.indexOf(":") + 3);
+				} catch (Exception e) { 
+					System.out.println("time error");
+					e.printStackTrace();
+					System.out.println(paragraph);
+					numTimeSkipped++;
+				}
+				
+				try { 
+					String afterLocation = paragraph.substring(paragraph.indexOf("Location: ")+10); 
+					if (afterLocation.contains("(")) { 
+						location = afterLocation.substring(0, afterLocation.indexOf("("));
+					} else { 
+						if (afterLocation.indexOf("Summary") == -1){
+							if (afterLocation.indexOf("Disposition") == -1) { 
+								location = afterLocation.substring(0, afterLocation.indexOf("  "));
+							} else {
+								location = afterLocation.substring(0, afterLocation.indexOf("Disposition"));
+							}
+						} else {
+							location = afterLocation.substring(0, afterLocation.indexOf("Summary"));
+						}
+					}
+				} catch (Exception e) { 
+					System.out.println("location error");
+					e.printStackTrace();
+					System.out.println(paragraph);
+					numLocationSkipped++;
+				}
+				try { 
+					summary = paragraph.substring(paragraph.indexOf("Summary: ") + 9);
+				} catch (Exception e) { 
+					System.out.println("summary error"); 
+					System.out.println(paragraph);
+					numSummarySkipped++;
+				}
+				
+			
 				Crime crime = new Crime(type, date, time, location, summary); 
 				crimes.add(crime);
+
 			}
-			
+
 			return crimes; 
-			
+
 		} catch (FailingHttpStatusCodeException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -134,19 +210,65 @@ public class Scraper {
 
 	}
 
-	public static void recordCrimes(List<Crime> crimes) { 
-		for (int i = 0; i < crimes.size(); i++) { 
-			
-			
-		}
-	}
-	
 
 	public static void main(String[] args) { 
-		
-		
-		
-		scrape(4, 2003); // month is index 1 down. Ex: March is denoted as 2 
+		XSSFWorkbook workbook = new XSSFWorkbook(); 
+		XSSFSheet sheet = workbook.createSheet("crime"); 
+		Row row = sheet.createRow(0);
+		row.createCell(0).setCellValue("date");
+		row.createCell(1).setCellValue("time");
+		row.createCell(2).setCellValue("location");
+		row.createCell(3).setCellValue("type");
+		row.createCell(4).setCellValue("description");
+		row.createCell(5).setCellValue("gotLocation");
+
+		int rowCount = 1; 
+
+		for (int i = 6; i<12; i++) { 
+			List<Crime> crimes = scrape(i, 2002);
+			for (int j = 0; j < crimes.size(); j++) { 
+				//System.out.println(i + " : " + j + " : " + crimes.size());
+				Crime crime = crimes.get(j); 
+				row = sheet.createRow(rowCount); 
+				rowCount++;
+				row.createCell(0).setCellValue(crime.date);
+				row.createCell(1).setCellValue(crime.time);
+				row.createCell(2).setCellValue(crime.location);
+				row.createCell(3).setCellValue(crime.type);
+				row.createCell(4).setCellValue(crime.summary);
+				row.createCell(5).setCellValue("false");
+			}
+		}
+
+		for (int i = 2003; i <= 2012; i++) { 
+			for (int j = 0; j < 12; j++) { 
+				List<Crime> crimes = scrape(j, i);
+				for (int k = 0; k < crimes.size(); k++) { 
+					Crime crime = crimes.get(k); 
+					row = sheet.createRow(rowCount); 
+					rowCount++;
+					row.createCell(0).setCellValue(crime.date);
+					row.createCell(1).setCellValue(crime.time);
+					row.createCell(2).setCellValue(crime.location);
+					row.createCell(3).setCellValue(crime.type);
+					row.createCell(4).setCellValue(crime.summary);
+					row.createCell(5).setCellValue("false");
+				}
+
+			}
+		}
+
+		try { 
+			// this Writes the workbook gfgcontribute 
+			FileOutputStream out = new FileOutputStream(new File("crimes.xlsx")); 
+			workbook.write(out); 
+			out.close(); 
+			System.out.println("crimes.xlsx written successfully on disk. Date: " + numDateSkipped + " Time: " + numTimeSkipped + " Location: " + numLocationSkipped + " Summary: " + numSummarySkipped); 
+		} 
+		catch (Exception e) { 
+			e.printStackTrace(); 
+		} 
+
 	}
 
 
